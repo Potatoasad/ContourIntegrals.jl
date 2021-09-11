db37de92ff4bd09ffe90db82e27c567db9e1bcab
Asad Hussain <asadh@utexas.edu>
2021-09-11 18:52:19 -0500

    added absolute tolerance

--- src/Integration.jl
+++ src/Integration.jl
@@ -1,5 +1,5 @@
 
-function Integrate(func,contour; rtol=1e-8)
+function Integrate(func,contour; rtol=1e-8, atol=0.0)
     f = TransformIntegrand(func,contour)
-    integral,error = quadgk(f,0,1,rtol=rtol)
+    integral,error = quadgk(f,0,1;rtol=rtol, atol=atol)
 end
