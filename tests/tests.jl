615118f4e931766fe82578bfe45f195de9c505c8
Asad Hussain <asadh@utexas.edu>
2021-09-11 00:08:36 -0500

    Managed to write the whole package between pushes

---
+++ tests/tests.jl
@@ -0,0 +1,10 @@
+L = LineSegment(0,2*π)
+@test Integrate(x -> sin(x)^2,L)[1] |> real ≈ π
+
+Semi = SemiInfiniteLine(0,1,true)
+@test Integrate(x -> exp(-x^2),Semi) |> real ≈ 0.886226925452758
+
+Full = InfiniteLine(0,1)
+@test Integrate(x -> exp(-x^2),Full) |> real ≈ 1.772453850905516
+
+@test Integrate(x -> exp(im*x),SemiInfiniteLine(0,1*im,true)) |> imag ≈ 1
