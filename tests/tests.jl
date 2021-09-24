0d84dd77ed539e29b2ba19c85a02f01159027372
Asad Hussain <asadhussain625@gmail.com>
2021-09-24 02:01:24 -0500

    updated tests

--- tests/tests.jl
+++ tests/tests.jl
@@ -4,7 +4,7 @@ L = LineSegment(0.0,2π,true)
 Semi = SemiInfiniteLine(0,1,true)
 @test Integrate(x -> exp(-x^2),Semi) |> real ≈ 0.886226925452758
 
-Full = InfiniteLine(0,1)
+Full = InfiniteLine(0,1,true)
 @test Integrate(x -> exp(-x^2),Full) |> real ≈ 1.772453850905516
 
 @test Integrate(x -> exp(im*x),SemiInfiniteLine(0,1*im,true)) |> imag ≈ 1
