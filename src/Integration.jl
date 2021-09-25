395a2e3037ae1f6e205518a5b6c176e021f26c41
Asad Hussain <asadh@utexas.edu>
2021-09-25 03:30:01 -0500

    set default abstol for integration to be not 0

--- src/Integration.jl
+++ src/Integration.jl
@@ -24,18 +24,18 @@ function Converter(f1s::Function,dims::Int)
     end
 end
 
-function Integrate(f::Function, C::Domain{d}; error_norm=Cubature.PAIRED,kws...) where d
+function Integrate(f::Function, C::Domain{d}; error_norm=Cubature.PAIRED, abstol=1e-10, kws...) where d
     f□ = let f=f, C=C; TransformIntegrand(f,C) end
     f□v = let f□ = f□, d=d;  Converter(f□ , d) end
-    (val,err) = pcubature_v(2, f□v, zeros(d), ones(d); error_norm=error_norm, kws...)
+    (val,err) = pcubature_v(2, f□v, zeros(d), ones(d); error_norm=error_norm, abstol=1e-10, kws...)
     val[1]+im*val[2] , sqrt(err[1]^2 + err[2]^2)
 end
 
-function Integrate(f::Function, C::SumDomain{d,p,T}; error_norm=Cubature.PAIRED,kws...) where {d,p,T}
+function Integrate(f::Function, C::SumDomain{d,p,T}; error_norm=Cubature.PAIRED,abstol=1e-10,kws...) where {d,p,T}
     valt = ComplexF64(0.0);
     errt = Float64(0.0);
     for i ∈ 1:p
-        val,error = Integrate(f,C.domains[i]; error_norm=Cubature.PAIRED,kws...)
+        val,error = Integrate(f,C.domains[i]; error_norm=Cubature.PAIRED, abstol=1e-10, kws...)
         valt += val
         errt += error^2
     end
